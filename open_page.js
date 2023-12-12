1a5d13fb09fa4943e69c1754087e6c4bea4c622e
HolloDoesCode <152638120+HolloDoesCode@users.noreply.github.com>
2023-12-12 12:07:16 -0700

    Add files via upload

---
+++ open_page.js
@@ -0,0 +1,179 @@
+function cookie_clicker() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/cookie-clicker/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function snake() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/snake/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function tic_tac_toe() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/tic-tac-toe/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function game_select() {
+  window.location.href = '/select_game.html';
+}
+
+function about() {
+  window.location.href = '/about.html';
+}
+
+function home() {
+  window.location.href = '/home.html';
+}
+
+function pong() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/pong/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function flappy_bird() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/flappy-bird/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function changelog() {
+  window.location.href = '/CHANGELOG.html';
+}
+
+function two_zero_four_eight() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/2048/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function etch_a_sketch() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/etch-a-sketch/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function mario_bros() {
+  window.location.href = '/mario-bros/start.html';
+}
+
+function geometry_dash() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/geometry-dash/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function fnaf(game_num) {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = `/fnaf/fnaf${game_num}.html`;
+  } else {
+    alert("Error");
+  }
+}
+
+function credits() {
+  window.location.href = '/credits';
+}
+
+function chrome_dino_game() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/chrome-dino-game/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function slope() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/slope/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function bloxorz_2() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/bloxorz-2/en.html';
+  } else {
+    alert("Error");
+  }
+}
+
+function suika() {
+  var enabled = true;
+  if (enabled == false) {
+    var reason = "";
+    alert(`Currently Disabled. Reason:\n${reason}`);
+  } else if (enabled == true) {
+    window.location.href = '/suika/en.html';
+  } else {
+    alert("Error");
+  }
+}
