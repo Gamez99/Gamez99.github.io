469cfe26747a8f29eab0916ccf1c1e2730cb8032
HolloDoesCode <152638120+HolloDoesCode@users.noreply.github.com>
2024-01-31 14:09:18 -0700

    Update popout_window.js

--- !assets/js/popout_window.js
+++ !assets/js/popout_window.js
@@ -1,4 +1,4 @@
 function open_popup_window() {
-  window.open("https://hollodoescode.github.io/home", "Games");
+  window.open("https://hollodoescode.github.io/home", "popup", "Games");
   this.location.close();
 }
