0cedaf2192a565f612fc8e2aa9cd0c3a9b9a4fd0
HolloDoesCode <152638120+HolloDoesCode@users.noreply.github.com>
2024-01-31 13:47:32 -0700

    Create popout_window.js

---
+++ !assets/js/popout_window.js
@@ -0,0 +1,9 @@
+function open_popup_window() {
+  window.open("https://hollodoescode.github.io/home", "Games");
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.altKey && and event.key == '`') {
+    open_popup_window();
+  }
+});
